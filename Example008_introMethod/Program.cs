657aa7c51fa4c77b8b1f3084ad26e8016b3bf97a
DarkWolf_ <whitejaguar08@gmail.com>
2022-04-26 21:12:41 +0300

    Complete_update

--- Example008_introMethod/Program.cs
+++ Example008_introMethod/Program.cs
@@ -8,7 +8,7 @@
 
 int a1 = 25;
 int b1 = 51;
-int c1 = 24;
+int c1 = 7774;
 int a2 = 12;
 int b2 = 10;
 int c2 = 22;
@@ -16,9 +16,14 @@ int a3 = 64;
 int b3 = 23;
 int c3 = 28;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+
+int max = Max(
+	Max(a1, b1, c1),
+	Max(a2, b2, c2),
+	Max(a3, b3, c3));
 Console.WriteLine(max);
 
