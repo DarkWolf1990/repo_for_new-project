46c0acb0713e4843a24bab3d1a0e726ec0fb03d2
DarkWolf_ <whitejaguar08@gmail.com>
2022-04-26 21:03:19 +0300

    Complete

--- Example008_introMethod/Program.cs
+++ Example008_introMethod/Program.cs
@@ -1,4 +1,4 @@
-﻿int max(int arg1, int arg2, int arg3)
+﻿int Max(int arg1, int arg2, int arg3)
 {
 	int result = arg1;
 	if(arg2 > result) result = arg2;
@@ -12,13 +12,13 @@ int c1 = 24;
 int a2 = 12;
 int b2 = 10;
 int c2 = 22;
-int a3 = 13;
+int a3 = 64;
 int b3 = 23;
 int c3 = 28;
 
-int max1 = max(a1, b1, c1);
-int max2 = max(a2, b2, c2);
-int max3 = max(a3, b3, c3);
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
 int max = Max(max1, max2, max3);
 Console.WriteLine(max);
 
